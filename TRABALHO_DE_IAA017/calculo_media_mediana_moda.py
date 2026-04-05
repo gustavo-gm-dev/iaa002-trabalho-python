8280f3ec15f9810ab06e3c5c47a937d245abc874
Matheus Henrique Miranda <matheushmiranda.dev@gmail.com>
2026-04-05 11:25:50 -0300

    Add código de limpeza em questão 2.a

--- TRABALHO_DE_IAA017/calculo_media_mediana_moda.py
+++ TRABALHO_DE_IAA017/calculo_media_mediana_moda.py
@@ -3,12 +3,18 @@ import pandas as pd
 # Ler o arquivo Excel
 df = pd.read_excel("Dados_Sneakers_World3.xlsx", sheet_name="Sales")
 
+# Limpar nomes das colunas
+df.columns = df.columns.str.strip()
+
 # Garantir que são números
 df["Sales"] = pd.to_numeric(df["Sales"], errors="coerce")
 df["Quantity"] = pd.to_numeric(df["Quantity"], errors="coerce")
 
-# Remover nulos (caso tenha sobrado algo)
+# Remover nulos
 df = df.dropna(subset=["Sales", "Quantity"])
+
+# Remover duplicatas
+df = df.drop_duplicates()
 
 # Calcular medidas
 resultados = {
